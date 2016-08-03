3cb15b8d242458b2520e213336f1b27065fe9b40
Sita Liu <chsliu@gmail.com>
2016-08-04 02:10:20 +0800

    Automated commit at 2016/08/04 週四  上午 02:10   on W1

--- sendemail.gitsync.bat
+++ sendemail.gitsync.bat
@@ -1,6 +1,38 @@
 @echo off
+
 REM =================================
-set path=C:\Windows\system32;%path%;%~dp0\..\utility
+goto :main
+
+REM =================================
+REM call :COUNTLINE <linefile>
+REM call :COUNTLINE temp.txt
+REM =================================
+:COUNTLINE
+for /f %%a in ('type "%1"^|find "" /v /c') do set /a cnt=%%a
+
+exit /b
+
+
+:INTERNETCHECK
+echo checking internet connection
+ping www.google.com -n 1 -w 1000 >NUL
+REM cls
+if errorlevel 1 (
+	set internet=Not connected to internet
+	goto :EOF
+) else (
+	set internet=Connected to internet
+)
+
+echo %internet%
+
+exit /b
+
+
+REM =================================
+:main
+REM =================================
+set path=C:\Windows\system32;%path%;%~dp0\..\bin
 
 REM =================================
 set MyDate=
@@ -9,6 +41,7 @@ set TODAY=%MyDate:~0,4%-%MyDate:~4,2%-%MyDate:~6,2%
 set MONTH=%MyDate:~0,4%-%MyDate:~4,2%
 
 REM =================================
+mkdir %temp% >nul 2>>&1
 set LOG1=%temp%\%~n0-%COMPUTERNAME%-%TODAY%.txt
 set TXT1=%temp%\%~n0.txt
 set LINE=%temp%\%~n0-line.txt
@@ -67,7 +100,7 @@ REM =================================
 copy %0 %TXT1% >nul
 
 if defined ALARM (
-sendemail -s msa.hinet.net -f egreta.su@msa.hinet.net -t chsliu@gmail.com -u [LOG] %COMPUTERNAME% %~n0 -m %0 -a %LOG1% %TXT1%
+sendemail -s msa.hinet.net -f egreta.su@msa.hinet.net -t chsliu@gmail.com -u [LOG] %COMPUTERNAME% %~n0 ERROR -m %0 -a %LOG1% %TXT1%
 )
 
 type %LOG1%
@@ -79,32 +112,3 @@ REM =================================
 rem pause
 
 C:\Windows\System32\timeout.exe 10
-
-REM =================================
-
-goto :EOF
-
-REM =================================
-REM call :COUNTLINE <linefile>
-REM call :COUNTLINE temp.txt
-REM =================================
-:COUNTLINE
-for /f %%a in ('type "%1"^|find "" /v /c') do set /a cnt=%%a
-
-exit /b
-
-:INTERNETCHECK
-echo checking internet connection
-ping www.google.com -n 1 -w 1000 >NUL
-REM cls
-if errorlevel 1 (
-	set internet=Not connected to internet
-	goto :EOF
-) else (
-	set internet=Connected to internet
-)
-
-echo %internet%
-
-exit /b
-
